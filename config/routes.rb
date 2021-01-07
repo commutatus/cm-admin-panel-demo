27f220666d0df7ee76faf9419cc238b64ef85bd1
barath46 <barath@commutatus.com>
2021-01-07 16:21:30 +0530

    feat: Buttons UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'sign_in' => 'welcome#sign_in'
   get 'sign_up' => 'welcome#sign_up'
   get 'reset_password' => 'welcome#reset_password'
+  get '/components' => 'components#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
