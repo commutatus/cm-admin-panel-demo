f68a29e5cab3718cc83b8ed961d3eeab7f701072
anbublacky <anbublacky@gmail.com>
2022-05-14 12:41:07 +0530

    fix: Sign in pages have been added with basic design.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,5 @@ Rails.application.routes.draw do
       root 'devise/sessions#new'
   end
   mount CmAdmin::Engine => "/cm_admin"
-  get 'sign_in' => 'welcome#sign_in'
-  get 'sign_up' => 'welcome#sign_up'
-  get 'reset_password' => 'welcome#reset_password'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
