66dbd57271503e1326dbe50a3c09ac7cce90ad43
anbublacky <anbublacky@gmail.com>
2022-07-18 22:12:44 +0530

    fix: removed unwanted method

--- app/helpers/cm_admin/custom_helper.rb
+++ app/helpers/cm_admin/custom_helper.rb
@@ -1,8 +1,5 @@
 module CmAdmin
   module CustomHelper
-    def print_name
-      ['name']
-    end
     
     def course_collection
       ::Course.only_beginner.map {|course| [course.title, course.id]}
